f0831fbd04ce147fcf7f0fb5750e5e175e334788
Joseph Joslin <73438491+jjoslin07@users.noreply.github.com>
2022-08-16 11:59:26 -0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import ChatPage from './components/ChatPage';
 import socketIO from 'socket.io-client';
+
 const socket = socketIO.connect('http://localhost:4000');
 function App() {
 	return (
-		<div>
-			<p>Hello World!</p>
-		</div>
+		<BrowserRouter>
+			<div>
+				<Routes>
+					<Route path="/" element={<Home socket={socket} />}></Route>
+					<Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+				</Routes>
+			</div>
+		</BrowserRouter>
 	);
 }
 
